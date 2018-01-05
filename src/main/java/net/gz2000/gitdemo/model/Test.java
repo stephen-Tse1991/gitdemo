709cd16b23b94d8f68fd60de998c18ac85bd5fd0
xiehaichao <xiehc@gz2000.net>
2018-01-05 16:46:00 +0800

    修改02

--- src/main/java/net/gz2000/gitdemo/model/Test.java
+++ src/main/java/net/gz2000/gitdemo/model/Test.java
@@ -6,8 +6,7 @@ package net.gz2000.gitdemo.model;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-	System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-	System.out.println("cccccccccccccccccccccccccccccccc");
-
+		System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+		System.out.println("cccccccccccccccccccccccccccccccc");
     }
 }
