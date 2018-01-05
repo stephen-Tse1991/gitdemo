39e395158fa7d9f9d07fd9035cbb1cb579794dd1
xiehaichao <xiehc@gz2000.net>
2018-01-05 16:23:12 +0800

    初始化好项目。

---
+++ src/main/java/net/gz2000/gitdemo/model/Test.java
@@ -0,0 +1,10 @@
+package net.gz2000.gitdemo.model;
+
+/**
+ * Created by stephen on 2018/01/05.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    }
+}
