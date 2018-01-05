55f5b41b962d70dee8f498daee60e8b52efe8e0e
stephen-Tse1991 <30787017+stephen-Tse1991@users.noreply.github.com>
2018-01-05 16:43:08 +0800

    Update Test.java

--- src/main/java/net/gz2000/gitdemo/model/Test.java
+++ src/main/java/net/gz2000/gitdemo/model/Test.java
@@ -6,6 +6,8 @@ package net.gz2000.gitdemo.model;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-		System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+	System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+	System.out.println("cccccccccccccccccccccccccccccccc");
+
     }
 }
